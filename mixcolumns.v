594a7c3d672281ea4b9eedec2231afe8aa6f02e8
Rajat Katepallewar <rajatkats124@gmail.com>
2024-11-06 22:13:36 +0530

    Update mixcolumns.v

--- mixcolumns.v
+++ mixcolumns.v
@@ -1,62 +1,46 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08/30/2024 12:29:57 AM
-// Design Name: 
-// Module Name: mixcolumns
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module mixcolumns(
-    input [127:0] state_in_col,
-    output[127:0] state_out_col
-    );
-    
+    input [127:0] state_in,
+    output [127:0] state_out
+);
 
-function [7:0] mb2; //multiply by 2
-	input [7:0] x;
-	begin 
-			/* multiplication by 2 is shifting on bit to the left, and if the original 8 bits had a 1 @ MSB,
-			xor the result with {1b}*/
-			if(x[7] == 1) mb2 = ((x << 1) ^ 8'h1b);
-			else mb2 = x << 1; 
-	end 	
+function [7:0] xtime;
+    input [7:0] x;
+    begin
+        xtime = (x << 1) ^ ((x & 8'h80) ? 8'h1b : 8'h00);
+    end
 endfunction
 
-function [7:0] mb3; //multiply by 3
-	input [7:0] x;
-	begin 
-			
-			mb3 = mb2(x) ^ x;
-	end 
+function [7:0] mul_by_3;
+    input [7:0] x;
+    begin
+        mul_by_3 = xtime(x) ^ x;
+    end
 endfunction
 
+function [31:0] mix_column;
+    input [31:0] col;
+    reg [7:0] b0, b1, b2, b3;
+    reg [7:0] h0, h1, h2, h3;
+    begin
+        b0 = col[31:24];
+        b1 = col[23:16];
+        b2 = col[15:8];
+        b3 = col[7:0];
+        
+        h0 = xtime(b0) ^ mul_by_3(b1) ^ b2 ^ b3;
+        h1 = b0 ^ xtime(b1) ^ mul_by_3(b2) ^ b3;
+        h2 = b0 ^ b1 ^ xtime(b2) ^ mul_by_3(b3);
+        h3 = mul_by_3(b0) ^ b1 ^ b2 ^ xtime(b3);
+
+        mix_column = {h0, h1, h2, h3};
+    end
+endfunction
 
 genvar i;
-
-generate 
-for(i=0;i< 4;i=i+1) begin : m_col
-
-	assign state_out_col[i*32 +:8]= mb2(state_in_col[i*32+:8]) ^ mb3(state_in_col[(i*32 + 8)+:8]) ^ state_in_col[(i*32 + 16)+:8] ^ state_in_col[(i*32 + 24)+:8];
-	assign state_out_col[(i*32 + 8)+:8]= state_in_col[i*32+:8] ^ mb2(state_in_col[(i*32 + 8)+:8]) ^ mb3(state_in_col[(i*32 + 16)+:8]) ^ state_in_col[(i*32 + 24)+:8];
-	assign state_out_col[(i*32 + 16)+:8]= state_in_col[i*32+:8] ^ state_in_col[(i*32 + 8)+:8] ^ mb2(state_in_col[(i*32 + 16)+:8]) ^ mb3(state_in_col[(i*32 + 24)+:8]);
-    assign state_out_col[(i*32 + 24)+:8]= mb3(state_in_col[i*32+:8]) ^ state_in_col[(i*32 + 8)+:8] ^ state_in_col[(i*32 + 16)+:8] ^ mb2(state_in_col[(i*32 + 24)+:8]);
-
-end
-
+generate
+    for (i = 0; i < 4; i = i + 1) begin : mixcols
+        assign state_out[(i*32) +: 32] = mix_column(state_in[(i*32) +: 32]);
+    end
 endgenerate
-    
+
 endmodule
